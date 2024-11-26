5e1863855309e19f3a5b31e18ccb11dc7759add0
alfi <142280813+alficlanjr@users.noreply.github.com>
2024-11-26 11:29:28 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="~/assets/img/Logo.png" alt="Logo" width="65" height="57" class="d-inline-block align-text-bootom">
+      SMKN 4 TASIKMALAYA
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Profle
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="profile/">sejarah</a></li>
+            <li><a class="dropdown-item" href="profile/visi">visi & misi</a></li>
+            <li><a class="dropdown-item" href="#">struktur organisasi</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Guru</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pasilitas</a>
+        </li>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</template>
+<style>
+.collapse{
+    padding-left: 68% ;
+}
+</style>
---
+++ struktur/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="col-6">
+            <img src="../assets/img/guru.jpg" alt="">
+        </div>
+    </div>
+</template>
