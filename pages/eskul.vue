1b6fee7bc0356dc323965ddc33034df88369b2cf
alfi <142280813+alficlanjr@users.noreply.github.com>
2024-12-24 14:51:21 +0700

    Add files via upload

---
+++ pages/eskul.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="row">
+        <div class="col-5">
+            <div class="row row-cols-1 row-cols-md-1 g-2">
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/basket.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Basket</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/bultang.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Bulu Tangkis</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="..//assets/img/cinema.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Cinematograpy</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/english.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">English Club</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/pmr.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">PMR</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/laskar.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Laskar Pustaka</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/paskib.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Paskibra</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/plh.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">PLH</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/pks.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">PKS</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/futsal.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Futsal</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/pramuka.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Pramuka</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src="../assets/img/voly.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Voly</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,105 @@
 <template>
-       <div>
-        <div class="card" style="width: 10rem;">
-  <img src="../assets/img/kepsek.png" class="card-img-top" alt="">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+  <div class="container-fluid">
+    <div class="p-5">
+      <div class="card p-5">
+        <div class="row">
+          <div class="col-5">
+            <img src="../assets/img/kpl.png" class="card-img-top" alt="">
+            <div class="card-body">
+              <h2 class="card-text text-center">KEPALA SEKOLAH</h2>
+            </div>
+          </div>
+          <div class="col-7 pt-5">
+            <h3>Sambutan Kepala Sekolah</h3>
+
+            Assalamualaikum Warahmatullahi Wabarakatuh
+            Selamat datang di SMKN 4 Tasikmalaya. Segala puji dan syukur kita panjatkan kehadirat Allah SWT, semoga kita
+            semua ada dalam lindungan-Nya. Dan atas perkenan-Nya pula kami dapat menghadirkan website SMK Negeri 4
+            Tasikmalaya ini. Kami berharap dengan adanya website di SMK Negeri 4 Tasikmalaya ini para pengunjung dapat
+            mengenal lebih jauh tentang sekolah kami sehingga dapat mempererat tali silaturrahmi antara sekolah dengan
+            masyarakat demi kemajuan kita bersama. Tiada gading yang tak retak, website kami ini masih dalam proses
+            pengembangan, masih banyak kekurangan yang harus kami perbaiki. Kritik dan sarannya yang membangun sangat
+            kami harapkan untuk pengembangan ke depan.
+
+            Akhirnya, saya mengucapkan terimakasih yang sebesar-besarnya kepada semua pihak yang tidak dapat disebutkan
+            satu segala bantuan dan persatu atas fasilitasnya yang telah diberikan semoga semua yang kita lakukan
+            bermanfaat bagi masyarakat. Wassalamu'alaikum Warahmatullahi Wabarakatuh.
+          </div>
+        </div>
+      </div>
     </div>
+    <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="../assets/img/guru.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img/gru.jpeg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="../assets/img/guruu.jpeg" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching"
+        data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching"
+        data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <div class="judul m-5 text-center">
+      <h1>KOMPOTENSI KEAHLIAN</h1>
+    </div>
+    <div class="row">
+      <div class="col-4">
+        <img src="../assets/img/tjkt.jpeg" class="rounded" alt="...">TEKNIK JARINGAN KOMPUTER & TELEKOMONIKASI
+      </div>
+      <div class="col-4">
+        <img src="../assets/img/pplg.jpeg" class="rounded" alt="...">PENGEMBANGAN PERANGKAT LUNAK DAN GIM
+      </div>
+      <div class="col-4">
+        <img src="../assets/img/tbsm.jpeg" class="rounded" alt="...">TEKNIK BISNIS SEPEDA MOTOR
+      </div>
+      <div class="col-4">
+        <img src="../assets/img/dkv.jpeg" class="rounded" alt="...">DESAIN KOMUNIKASI VISUAL
+      </div>
+      <div class="col-4">
+        <img src="../assets/img/toi.jpeg" class="rounded" alt="...">TEKNIK OTOMASI INDUSTRI
+      </div>
+    </div>
+  </div>
+  <div class="teacher m-5 text-center">
+    <h1>DATA JUMLAH GURU & MURID</h1>
+    <div class="row">
+      <div class="col-4  d-flex">
+        <div class="card">
+          <div class="card-body">
+            <h3>GURU</h3>
+            82
+          </div>
+        </div>
+        <div class="card">
+          <div class="card-body">
+            <h3>MURID</h3>
+            1400
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<style scoped>
+img {
+  width: 100%;
+}
+
+.card {
+  width: 75%;
+  height: 50%;
+  margin-right: 30%;
+}
+</style>
